b7b5a1d50a9d8f5bfc8adfccf9caca6894a4efe7
Emanuel Passinato <emanuelborges123@hotmail.com>
2019-09-01 11:47:19 -0300

    Inicio do relatorio

---
+++ Pactolo/scr/dominio/Evento.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pactolo.scr.dominio {
+    class Evento {
+        public string NomeSesssao { get; set; }
+        public string NomeCC  { get; set; }
+        public long pontosGanhos { get; set; }
+        DateTime horaEvento { get; set; }
+    }
+}

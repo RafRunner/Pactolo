bb6d67657fb18d641e685767563df6f511db6880
Emanuel Passinato <emanuelborges123@hotmail.com>
2019-09-01 21:15:03 -0300

    quase fim

--- Pactolo/scr/dominio/Sessao.cs
+++ Pactolo/scr/dominio/Sessao.cs
@@ -10,14 +10,13 @@ namespace Pactolo.scr.dominio {
 	//Me parece pelo menu que o examinador pode cadastrar uma Contingencia Instritucional aqui tambem, 
 	//mas não faz sentido. Validar!
 	class Sessao : ElementoDeBanco {
-        //Parece que ele coloca nome nas seções mas no layout da tela não teria como
+
         protected string nome;
 		public string Nome {
 			get => nome;
 			set => nome = StringUtils.ValideNaoNuloNaoVazioENormalize(value, "Nome");
 		}
 
-		//TODO ver como que vai ficar a referência a esses objetos. QUando der set na lista provalvel que vai ter que salvar em uma lista de ids
 		public List<ContingenciaColateral> CCs { get; set; }
 
 		private string ordemExposicao;
@@ -25,12 +24,8 @@ namespace Pactolo.scr.dominio {
 			get => ordemExposicao;
 			set => EOrdemExposicao.ParseAndValidate(value);
 		}
-		//Estudar se vale a pena criar objetos ao invez dessa declaração direta
-		public Boolean CriterioNumeroTentativas { get; set; }
 		public int NumeroTentativas { get; set; }
-		public Boolean CriterioDuracaoSegundos { get; set; }
 		public long DuracaoSegundos { get; set; }
-		public Boolean CriterioAcertosConcecutivos { get; set; }
 		public int AcertosConcecutivos { get; set; }
 	}
 }
